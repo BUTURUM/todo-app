a82cb506707a236ae4e46fb56ced25dde7f494b4
BUTURUM <75197972+BUTURUM@users.noreply.github.com>
2024-04-01 21:09:46 +0100

    Add todo item form was created

---
+++ components/Add-Todo.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="add-todo box" @submit.prevent="submit">
+    <div class="add-todo__figure"></div>
+    <input
+      class="add-todo__input"
+      placeholder="Create Todo..."
+      type="text"
+      v-model="title"
+    >
+  </form>
+</template>
+<script>
+  import '../style/todo.css';
+
+  export default {
+    methods: {
+      submit(){
+        this.$emit('add-item', this.title);
+        this.title = ''
+      }
+    },
+    data(){
+      return {title: ''}
+    }
+  }
+</script>
--- components/App.vue
+++ components/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="wrapper">
     <Header></Header>
+    <AddTodo @add-item="console.log($event)"/>
   </div>
 </template>
 <script>
   import '../style/general.css';
+  import AddTodo from './Add-Todo.vue';
 
   import Header from './Header.vue';
 
   export default {
-    components: {Header}
+    components: { Header, AddTodo }
   }
 </script>
