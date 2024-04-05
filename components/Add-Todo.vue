56919a46db0123b664de1c036e6ef2b4371e50a7
BUTURUM <75197972+BUTURUM@users.noreply.github.com>
2024-04-05 20:00:16 +0100

    Improve add todo form
    * Add plus button, which adds todos. Before the only way to do this was
    by pressing "Enter" which isn't intuitive.
    * Other minor changes.

--- components/Add-Todo.vue
+++ components/Add-Todo.vue
@@ -1,16 +1,23 @@
 <template>
-  <form class="add-todo box" @submit.prevent="submit">
-    <div class="add-todo__figure"></div>
-    <input
-      class="add-todo__input"
-      placeholder="Create Todo..."
-      type="text"
-      v-model="title"
-    >
+  <form class="box" @submit.prevent="submit">
+    <label class="add-todo">
+      <div class="add-todo__figure"></div>
+      <input
+        class="add-todo__input"
+        placeholder="Create Todo..."
+        type="text"
+        v-model="title"
+      >
+      <button class="icon-animated-btn" type="submit">
+        <PlusIcon />
+      </button>
+    </label>
   </form>
 </template>
 <script>
   import '../style/todo.css';
+
+  import PlusIcon from '../img/bx-plus.svg';
 
   export default {
     emits: ['add-item'],
@@ -25,6 +32,7 @@
     },
     data(){
       return {title: ''}
-    }
+    },
+    components: {PlusIcon}
   }
 </script>
